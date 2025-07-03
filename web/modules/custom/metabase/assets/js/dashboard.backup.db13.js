1d98e6cc8219aed5361683b5a798ef58cee0b335
Diego Restrepo <diegoluisr@gmail.com>
2025-07-03 15:42:12 -0500

    Updating metabase module

---
+++ web/modules/custom/metabase/assets/js/dashboard.backup.db13.js
@@ -0,0 +1,52 @@
+(function () {
+  'use strict';
+  setInterval(() => {
+    document.querySelectorAll('[data-testid="dashcard"]').forEach(container => {
+      const titleElement = container.querySelector('div[data-testid="scalar-title"]>h3>div');
+      const valueElement = container.querySelector('div[data-testid="scalar-container"]>span>h1');
+      if (!titleElement || !valueElement) {
+        return;
+      }
+      const text = titleElement.innerText.trim();
+      const value = valueElement.innerText.trim();
+      if (text.includes('-o-')) {
+        container.parentNode.classList.add("container-small");
+        if (value.includes('-') && !container.classList.contains("decrease")) {
+          container.classList.add("decrease");
+          valueElement.textContent =valueElement.textContent.replace('-', '');
+        }
+        else if (value.includes('+') && !container.classList.contains("increase")) {
+          container.classList.add("increase");
+          valueElement.textContent = valueElement.textContent.replace('+', '');
+        }
+        else if (!container.classList.contains("decrease") && !container.classList.contains("increase") && !container.classList.contains("equals")) {
+          container.classList.add("equals");
+        }
+      }
+      else {
+        if (!container.classList.contains("dashcard")) {
+          container.classList.add("dashcard");
+
+          if (!valueElement.textContent.includes('|')) {
+            return;
+          }
+
+          const parts = valueElement.textContent.split('|');
+
+          if (parts.length < 2) {
+            return;
+          }
+
+          const enhancedHTML = parts[0] + '<span>' + parts[1] + '</span>';
+          valueElement.innerHTML = enhancedHTML;
+
+          if (parts.length == 3) {
+            titleElement.innerHTML = titleElement.innerHTML.replace('###', parts[2]);
+          }
+        }
+      }
+    }
+    );
+  }, 50);
+}
+)();
