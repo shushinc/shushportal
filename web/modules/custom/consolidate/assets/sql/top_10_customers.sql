a14cc840e8de1aec1d59cc620dfbcca99915167f
Diego Restrepo <diegoluisr@gmail.com>
2025-09-01 21:33:14 -0500

    Updating consolidate module

---
+++ web/modules/custom/consolidate/assets/sql/top_10_customers.sql
@@ -0,0 +1,9 @@
+SELECT CAST(SUM(node__field_success_api_volume_in_mil.field_success_api_volume_in_mil_value) AS SIGNED) AS `total` from node
+LEFT JOIN node__field_date ON node.nid = node__field_date.entity_id
+LEFT JOIN node__field_success_api_volume_in_mil on node.nid = node__field_success_api_volume_in_mil.entity_id
+WHERE `type` = 'analytics'
+AND node__field_date.bundle = 'analytics'
+AND node__field_date.field_date_value >= :start_date
+AND node__field_date.field_date_value <= :end_date
+GROUP BY `type`
+;
