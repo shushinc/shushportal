f4de45c5eacd87536ebf5989f532218b3bbd1cef
Eric Vieira <ericf@shush.pw>
2026-01-08 22:56:58 -0300

    PANTHER2-73: Removing business logic from Controller and creating a service for handle this.

--- web/modules/custom/sam/src/Controller/SsoController.php
+++ web/modules/custom/sam/src/Controller/SsoController.php
@@ -11,6 +11,7 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Drupal\sam\Service\IdentityManager;
 
 /**
  * Controller for SSO authentication.
@@ -32,6 +33,13 @@ class SsoController extends ControllerBase {
   protected $messenger;
 
   /**
+   * The IdentityManager service.
+   *
+   * @var \Drupal\sam\Service\IdentityManager
+   */
+  protected $identityManager;
+
+  /**
    * Constructs a new SsoController object.
    *
    * @param \Drupal\sam\SsoProviderManager $provider_manager
@@ -39,9 +47,10 @@ class SsoController extends ControllerBase {
    * @param \Drupal\Core\Messenger\MessengerInterface $messenger
    *   The messenger service.
    */
-  public function __construct(SsoProviderManager $provider_manager, MessengerInterface $messenger) {
+  public function __construct(SsoProviderManager $provider_manager, MessengerInterface $messenger, IdentityManager $identity_manager) {
     $this->providerManager = $provider_manager;
     $this->messenger = $messenger;
+    $this->identityManager = $identity_manager;
   }
 
   /**
@@ -50,7 +59,8 @@ class SsoController extends ControllerBase {
   public static function create(ContainerInterface $container) {
     return new static(
       $container->get('sam.provider_manager'),
-      $container->get('messenger')
+      $container->get('messenger'),
+      $container->get('sam.identity_manager')
     );
   }
 
@@ -113,15 +123,11 @@ class SsoController extends ControllerBase {
       $user_data = $provider_instance->handleCallback($request);
 
       // Find or create user.
-      $account = $this->findOrCreateUser($user_data);
+      $account = $this->identityManager->resolveUser($userData);
 
       if ($account) {
         // Log in the user.
         user_login_finalize($account);
-
-        $this->messenger->addStatus($this->t('Successfully logged in via @provider.', [
-          '@provider' => $provider_instance->getName(),
-        ]));
 
         // Redirect to configured path or user profile.
         $config = $this->config('sam.settings');
@@ -144,54 +150,4 @@ class SsoController extends ControllerBase {
       return $this->redirect('user.login');
     }
   }
-
-  /**
-   * Finds an existing user or creates a new one based on SSO data.
-   *
-   * @param array $user_data
-   *   User data from SSO provider.
-   *
-   * @return \Drupal\user\Entity\User|null
-   *   The user account or NULL if creation failed.
-   */
-  protected function findOrCreateUser(array $user_data) {
-    $email = $user_data['email'] ?? '';
-
-    if (empty($email)) {
-      return NULL;
-    }
-
-    // Try to find existing user by email.
-    $existing_users = $this->entityTypeManager()
-      ->getStorage('user')
-      ->loadByProperties(['mail' => $email]);
-
-    if (!empty($existing_users)) {
-      return reset($existing_users);
-    }
-
-    // Create new user if auto-creation is enabled.
-    $config = $this->config('sam.settings');
-    if ($config->get('auto_create_users')) {
-      $user = User::create([
-        'name' => $user_data['name'] ?? $email,
-        'mail' => $email,
-        'status' => 1,
-        'access' => \Drupal::time()->getRequestTime(),
-      ]);
-
-      // Add any roles specified by the provider.
-      if (!empty($user_data['roles'])) {
-        foreach ($user_data['roles'] as $role) {
-          $user->addRole($role);
-        }
-      }
-
-      $user->save();
-      return $user;
-    }
-
-    return NULL;
-  }
-
 }
