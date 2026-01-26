64203adfec11ca38f38adefa8790d3bde6622fb7
girish06 <giridevelop@gmail.com>
2026-01-26 20:27:33 +0530

    pricing-discount-management-page

---
+++ web/modules/custom/zcs_api_attributes/src/Form/ManageDiscountPricing.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace Drupal\zcs_api_attributes\Form;
+
+use Drupal\Core\Database\Connection;
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Link;
+use Drupal\user\Entity\User;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Defines a basic custom form.
+ */
+class ManageDiscountPricing extends FormBase {
+
+
+  protected $database;
+
+  public function __construct(Connection $database) {
+    $this->database = $database;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('database'),
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'manage_pricing_history';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $header = [
+      'id' => $this->t('ID'),
+      'submit_by' => $this->t('Submitted By'),
+      'client_name' => $this->t('Client Name'),
+      'requested_date' => $this->t('Requested Date'),
+      'approver1_uid' => $this->t('Approver1 Email'),
+      'approver2_uid' => $this->t('Approver2 Email'),
+      'approver1_status' => $this->t('Approver1 Status'),
+      'approver2_status' => $this->t('Approver1 Status'),
+      'attribute_status' => $this->t('Status'),
+      'operations' => $this->t('Operations'),
+    ];
+
+
+
+    $query = $this->database->select('discount_pricing_page_data', 'dppd')
+    ->fields('dppd', ['id', 'submit_by','client_name', 'approver1_uid', 'approver2_uid', 'approver1_status', 'approver2_status', 'attribute_status', 'created'])
+    ->orderBy('updated', 'DESC') 
+    ->extend('Drupal\Core\Database\Query\PagerSelectExtender')
+    // 10 items per page
+    ->limit(10);
+    $results = $query->execute();
+
+  
+
+    $rows = [];
+    foreach ($results as $row) {
+      $statusSet = $this->database->select('attribute_status', 'as')
+        ->fields('as', ['id', 'status'])
+        ->execute()
+        ->fetchAll();
+      foreach ($statusSet as $status) {
+        $states[$status->id] = $status->status;
+      }
+
+      $rows[] = [
+        'id' => $row->id,
+        'submit_by' => $this->getUserMail($row->submit_by),
+        'client_name' => $row->client_name,
+        'requested_time' => date('M d, Y', $row->created),
+        'approver1_uid' => !empty($row->approver1_uid) ? $this->getUserMail($row->approver1_uid) : 'NA',
+        'approver2_uid' => !empty($row->approver2_uid) ? $this->getUserMail($row->approver2_uid) : 'NA',
+        'approver1_status' => $states[$row->approver1_status],
+        'approver2_status' => $states[$row->approver1_status],
+        'attribute_status' => $states[$row->attribute_status],
+        'url' => Link::createFromRoute('Delete', 'zcs_api_attributes.pricing_discount_delete', ['id' => $row->id]),
+      ];
+    }
+
+    $form['manage_api_attributes'] = [
+      '#type' => 'table',
+      '#header' => $header,
+      '#rows' => $rows,
+      '#empty' => $this->t('No API Attributes Found'),
+    ];
+
+    // Add pager.
+    $form['pager'] = ['#type' => 'pager'];
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getUserMail($uid) {
+    // Load the user entity.
+    $email = '';
+    $user = User::load($uid);
+    if ($user) {
+      $email = $user->getEmail();
+    }
+    return $email;
+  }
+
+}
